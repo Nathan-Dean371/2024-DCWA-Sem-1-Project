672369a981920810fe4b496c2146897463ce05cd
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2024-12-07 19:30:50 +0000

    Started on the student page. Got the connection to mySQL working.

--- DCWA-APP/index.js
+++ DCWA-APP/index.js
@@ -1,10 +1,51 @@
 const express = require('express')
+const pmysql = require('promise-mysql');
+require('dotenv').config()
 const path = require('path')
 const app = express()
 const port = 3004
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/students', (req, res) => {
+  
+  var connection = pmysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',   
+    password: 'password',
+    database: 'proj2024mysql'
+    }).then((p) =>
+    {
+        pool = p;
+        console.log('Connected to MySQL');
+        pool.query('SELECT * FROM student').then((data) =>
+          {
+              //console.log(data);
+              res.render('pages/students',
+              {
+                  studentArray: data
+              });
+          }).catch((err) => 
+          {
+              console.log('Error querying MySQL');
+              console.log(err);
+          });
+    }).catch((err) =>
+    {
+        console.log('Error connecting to MySQL');
+        console.log(err);   
+    });
+
+    
+
+    
+
 })
 
 app.listen(port, () => {
