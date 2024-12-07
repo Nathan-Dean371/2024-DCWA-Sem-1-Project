2b253c054a2a6e0f461f1ddee38069a48cf1feb5
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2024-12-07 14:05:46 +0000

    Added home page and the route to display.

--- DCWA-APP/index.js
+++ DCWA-APP/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3004
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(port, () => {
