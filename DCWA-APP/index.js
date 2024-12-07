6da7973e2d087b7711ade235edca78c2740873b1
Nathan Dean <G00423489@atu.ie>
2024-12-07 19:56:14 +0000

    Moved creation of the mySql connection pool to initial load of the app.

--- DCWA-APP/index.js
+++ DCWA-APP/index.js
@@ -4,47 +4,46 @@ require('dotenv').config()
 const path = require('path')
 const app = express()
 const port = 3004
+var pool;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+var connection = pmysql.createPool({
+  connectionLimit: 3,
+  host: 'localhost',
+  user: 'root',   
+  password: 'password',
+  database: 'proj2024mysql'
+  }).then((p) =>
+  {
+      pool = p;
+      console.log('Connected to MySQL');
+      
+  }).catch((err) =>
+  {
+      console.log('Error connecting to MySQL');
+      console.log(err);   
+  });
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.get('/students', (req, res) => {
-  
-  var connection = pmysql.createPool({
-    connectionLimit: 3,
-    host: 'localhost',
-    user: 'root',   
-    password: 'password',
-    database: 'proj2024mysql'
-    }).then((p) =>
-    {
-        pool = p;
-        console.log('Connected to MySQL');
-        pool.query('SELECT * FROM student').then((data) =>
+app.get('/students', (req, res) => 
+  {
+    pool.query('SELECT * FROM student').then((data) =>
+      {
+          console.log(data);
+          res.render('pages/students',
           {
-              //console.log(data);
-              res.render('pages/students',
-              {
-                  studentArray: data
-              });
-          }).catch((err) => 
-          {
-              console.log('Error querying MySQL');
-              console.log(err);
+              studentArray: data
           });
-    }).catch((err) =>
-    {
-        console.log('Error connecting to MySQL');
-        console.log(err);   
-    });
-
-    
-
-    
+      }).catch((err) => 
+      {
+          console.log('Error querying MySQL');
+          console.log(err);
+      });
 
 })
 
