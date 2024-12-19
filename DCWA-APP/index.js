ce02103076b0ff4200536ec91adf6a7d43f4f2b8
Nathan Dean <G00423489@atu.ie>
2024-12-19 14:40:40 +0000

    Completed the update sutdent functionality.
    Display correct errors messages.
    Redirect to student page when succesfully updated.

--- DCWA-APP/index.js
+++ DCWA-APP/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const pmysql = require('promise-mysql');
+const { check, validationResult } = require('express-validator');
 require('dotenv').config()
 const path = require('path')
 const app = express()
-const port = 3004
+const port = 3004;
 var pool;
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use(express.static('public'))
+
 app.set('view engine', 'ejs')
 
 var connection = pmysql.createPool({
@@ -49,8 +54,7 @@ app.get('/students/edit/:sid', (req, res) =>
 
   pool.query('SELECT * FROM student WHERE sid = ?', [req.params.sid]).then((data) =>
   {
-      console.log(data);
-      res.render('pages/editStudent',{student: data[0]});
+      res.render('pages/editStudent',{student: data[0], errors: undefined});
   }).catch((err) =>
   {
       console.log('Error querying MySQL');
@@ -58,6 +62,34 @@ app.get('/students/edit/:sid', (req, res) =>
   });
 });
 
+app.post('/students/edit/:sid', 
+        [
+          check("name").isLength({min:2}).withMessage("Name must be at least 2 characters long"),
+          check("age").isInt({min:18}).withMessage("Age must be 18 or older")
+        ],
+        (req, res) =>
+        {
+          const errors = validationResult(req);
+          if(!errors.isEmpty())
+          {
+            res.render('pages/editStudent',{student: req.body, errors: errors.errors});
+          }
+          else
+          {
+            //Update entry in the SQL database
+            pool.query('UPDATE student SET name = ?, age = ? WHERE sid = ?', [req.body.name, req.body.age, req.params.sid]).then((data) =>
+            {
+              res.redirect('/students');
+            }).catch((err) =>
+            {
+              console.log('Error querying MySQL');
+              console.log(err);
+            });
+          }
+        });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Nathan Dean - DCWA Project listening on port ${port}`)
 })
+
+
