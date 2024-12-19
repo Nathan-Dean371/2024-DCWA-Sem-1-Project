2d665184f58260ec44ad047328dd25490c3e7cff
Nathan Dean <G00423489@atu.ie>
2024-12-19 17:57:19 +0000

    Added the add student functionality and the grade display page.

--- DCWA-APP/index.js
+++ DCWA-APP/index.js
@@ -7,6 +7,7 @@ const app = express()
 const port = 3004;
 var pool;
 
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -30,7 +31,6 @@ var connection = pmysql.createPool({
       console.log('Error connecting to MySQL');
       console.log(err);   
   });
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
@@ -40,6 +40,7 @@ app.get('/students', (req, res) =>
     pool.query('SELECT * FROM student').then((data) =>
       {
           //console.log(data);
+          
           res.render('pages/students',{students: data});
       }).catch((err) => 
       {
@@ -47,6 +48,50 @@ app.get('/students', (req, res) =>
           console.log(err);
       });
 })
+
+app.get('/students/add', (req, res) => {
+  var emptyStudent = {sid: '', name: '', age: ''};
+  res.render('pages/addStudent',{errors: undefined, student : emptyStudent});
+})
+
+app.post('/students/add', 
+  [
+        check("sid").custom(value =>
+        {
+          const sid = value;
+          const sids = pool.query('SELECT * FROM student WHERE sid = ?', [sid]);
+          return sids.then((data) =>
+          {
+            if(data.length > 0)
+            {
+              return Promise.reject('SID already exists');
+            }
+          });
+        }),
+        check("sid").isLength(4).withMessage("SID must be 4 characters long"),
+        check("name").isLength({min:2}).withMessage("Name must be at least 2 characters long"),
+        check("age").isInt({min:18}).withMessage("Age must be 18 or older")
+  ],
+  (req, res) =>
+  {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+      res.render('pages/addStudent',{errors: errors.errors, student : req.body});
+    }
+    else
+    {
+      pool.query('INSERT INTO student (sid, name, age) VALUES (?, ?, ?)', [req.body.sid, req.body.name, req.body.age]).then((data) =>
+      {
+        res.redirect('/students');
+      }).catch((err) =>
+      {
+        console.log('Error querying MySQL');
+        console.log(err);
+      });
+    }
+    
+  });
 
 app.get('/students/edit/:sid', (req, res) =>
 {
@@ -87,6 +132,21 @@ app.post('/students/edit/:sid',
             });
           }
         });
+
+app.get('/grades', (req, res) =>
+{
+  
+  pool.query("SELECT student.name, module.name as mName, grade FROM student LEFT JOIN grade ON student.sid = grade.sid LEFT JOIN module ON grade.mid = module.mid ORDER BY student.name ASC, grade").then((data) =>
+  {
+      console.log(data);
+      res.render('pages/grades',{grades: data});
+  }).catch((err) =>
+  {
+      console.log('Error querying MySQL');
+      console.log(err);
+  });
+  
+});
 
 app.listen(port, () => {
   console.log(`Nathan Dean - DCWA Project listening on port ${port}`)
